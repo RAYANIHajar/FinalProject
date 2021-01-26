32fc1763cb713adb719b3ea1e8807605af3f37d4
RAYANIHajar <77999429+RAYANIHajar@users.noreply.github.com>
2021-01-26 21:01:20 +0100

    update

--- src/finalprojectjava/hotelController.java
+++ src/finalprojectjava/hotelController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package hotel;
+package finalprojectjava ;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -16,7 +16,7 @@ import javafx.scene.control.Label;
  *
  * @author HP
  */
-public class FXMLDocumentController implements Initializable {
+public class hotelController implements Initializable {
     
     @FXML
     private Label label;
